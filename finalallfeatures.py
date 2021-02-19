b7bb2754c94c7da4fd1551effef34a04c6413234
akshitag2009 <52240058+akshitag2009@users.noreply.github.com>
2021-02-20 00:56:00 +0530

    Add files via upload

--- finalallfeatures.py
+++ finalallfeatures.py
@@ -97,6 +97,20 @@ def nutrition(age=None):
         content6 = 'Try again.'
         return jsonify(content6)
 
+@app.route("/foodscan/<tags>")
+def foodScan(tags=None):
+  #thunkableoutput="table, back, orange, bed, ear, mouth"
+  split = tags.split(",")
+  foods = {'orange': 'Vitamin C - 53%, Carbohydrates - 12%, Protein - 0.9%',
+            'apple' : 'Carbohydrates - 14%, Protein - 0.3%' ,
+            'bread' : 'Carbohydrates - 49%, Protein - 9%,' ,
+            'cheese' : 'Carbohydrates - 1.3%, Protein = 25%, Fat = 33%,'}
+  
+  for food in split:
+    try:
+      return jsonify(f"{food}: {foods[food]}")
+    except:
+        pass
 
 @app.route('/examplesoffoods/<wants>')
 def foods(wants):
